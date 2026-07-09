3d56a6a833fdeba410072620ef0ccf46f14f8921
Bonobo Deployer <deployer@bonobocreativos.com>
2026-07-08 23:01:46 -0500

    Fix deployment path and bypass Jekyll

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
-  base: '/Elevation2/',
+  base: '/Elevation3/',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
